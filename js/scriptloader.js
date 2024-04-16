8ba7acbb09280a6f7bc8c52e92c3250d9adf9ffd
Anshay Yadav <anshaydev292@gmail.com>
2024-04-16 14:04:31 +0530

    kafi changes hai

--- js/scriptloader.js
+++ js/scriptloader.js
@@ -1,26 +1,6 @@
-$(document).ready(function () {
-    $("#view-profile-modal").load("modal-profile.html");
-    // $("#footer-placeholder").load("footer.html");
-    // setContent();
-    
-    // Add an event listener to the profile button
-    document.getElementById('profile-btn').addEventListener('click', function() {
-        // Call the function to show the profile modal
-        setContent();
-    });
-});
 
-function setContent(){
-    alert("hello");
-
-    // var modal = document.getElementById("view-profile-modal-new");
-    // $(modal).modal('show');
-    
-    // // Check if the modal element exists
-    // if (modal) {
-    //     // Use Bootstrap's modal function to show the modal
-        
-    // } else {
-       
-    // }
-}
+    fetch('header.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('header-placeholder').innerHTML = data;
+        });
